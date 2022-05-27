5433999cd579b9542bef8b29682131e392036326
shrutikhose <31467924+shrutikhose@users.noreply.github.com>
2022-05-27 16:45:41 +0530

    Add files via upload

---
+++ num1.py.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class MyNumbers:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+
+# In[ ]:
+
+
+
+
